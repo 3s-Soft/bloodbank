23fdad523ddcdf13a68b97e653667b1ecac6fc01
Md. Nahidul Islam <nahidulss@gmail.com>
2022-04-16 10:44:26 +0600

    sign up and sign page using template

--- Nahid/Admin/admin.go
+++ Nahid/Admin/admin.go
@@ -7,24 +7,58 @@ import (
 )
 
 var err error
-var temp *template.Template
+var btemp *template.Template
 
 func main() {
-	temp, err = template.ParseGlob("../public/*.gohtml")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	http.HandleFunc("/", home)
 
+	http.HandleFunc("/", home)
+	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/signin", signin)
 	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("../asset"))))
-	http.Handle("/resources2/", http.StripPrefix("/resources2/", http.FileServer(http.Dir("../node_modules"))))
+	//	http.Handle("/resources2/", http.StripPrefix("/resources2/", http.FileServer(http.Dir("../node_modules"))))
 	http.ListenAndServe(":8040", nil)
 	//fmt.Println("welcome")
-	
 
 }
 func home(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, `welcome to blood bank`)
-	temp.ExecuteTemplate(w, "index.gohtml", nil)
+	btemp, err = template.ParseFiles("templates/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	btemp.Execute(w, nil)
+
+}
+func signup(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, `welcome to blood bank`)
+	//btemp.ExecuteTemplate(w, "base.gohtml", nil)
+	fmt.Println("signup")
+	btemp, err = template.ParseFiles("templates/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	btemp, err = btemp.ParseFiles("webpage/signup.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	btemp.Execute(w, nil)
+
+}
+func signin(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, `welcome to blood bank`)
+	//	btemp.ExecuteTemplate(w, "base.gohtml", nil)
+
+	btemp, err = template.ParseFiles("templates/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	btemp, err = btemp.ParseFiles("webpage/signin.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	btemp.Execute(w, nil)
 
 }
