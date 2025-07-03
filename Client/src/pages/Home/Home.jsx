791358ebd9c6bb202e0508f13955f5c2c11696c2
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-07-04 02:13:13 +0600

    feat: add Hero component to Home page; include lucide-react for icons and update package dependencies

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Hero from '../../Components/HomePageComponents/Hero';
 
 const Home = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+            <Hero />
         </div>
     );
 };
