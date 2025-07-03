497098943e73bff3289ce6bb85ff29eee0c2b08c
Risan <borhansiddque19@gmail.com>
2025-07-04 02:29:26 +0600

    fix: remove shadow from Navbar component for a cleaner appearance

--- Client/src/Components/Navbar.jsx
+++ Client/src/Components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
   );
   return (
     <div className="bg-[#D1444A]">
-      <div className="navbar shadow-sm max-w-7xl mx-auto">
+      <div className="navbar max-w-7xl mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
