3450c1f40fcda1d88f3375731e4ece5f4d20ada8
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-07-04 16:12:24 +0600

    refactor: remove deprecated authentication and database connection logic, migrate to MongoDB

--- Server/routes/donors.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllDonors, addDonor } = require('../controllers/donorController');
-const { authenticate, authorize } = require('../auth/jwt');
-
-router.get('/', getAllDonors);
-router.post('/', authenticate, authorize(['donor']), addDonor);
-
-module.exports = router;
--- Server/routes/seekers.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllSeekers, addSeeker } = require('../controllers/seekerController');
-const { authenticate, authorize } = require('../auth/jwt');
-
-router.get('/', getAllSeekers);
-router.post('/', authenticate, authorize(['seeker']), addSeeker);
-
-module.exports = router;
